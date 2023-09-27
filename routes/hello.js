e75e3dc92cce36168e2708a3f92d4a47e8f10a65
pitasab6 <kitatetsu81@gmail.com>
2023-09-27 12:27:03 +0900

    ajaxを使って非同期通信の例を作った

--- routes/hello.js
+++ routes/hello.js
@@ -21,4 +21,13 @@ router.post('/', function(req, res, next) {
     res.render('hello', data);
 });
 
+router.post('/ajax', function(req, res, next) {
+    const result = {
+        id:req.body.id,
+        pass:req.body.pass,
+        message:'こんにちは、' + req.body.id + 'さん！'
+    }
+    res.send(result);
+});
+
 module.exports = router;
